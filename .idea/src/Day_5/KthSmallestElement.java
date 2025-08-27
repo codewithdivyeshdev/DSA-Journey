bcbcf539fa6b9b20b1b512afcf2774d3913abff8
Your Name <dabhidivyesh33@gmail.com>
2025-08-27 07:38:44 +0530

    Day 5

--- .idea/src/Day_5/KthSmallestElement.java
+++ .idea/src/Day_5/KthSmallestElement.java
@@ -1,8 +1,11 @@
 package Day_5;
-import java.util.ArrayList;
+import java.util.Arrays;
 public class KthSmallestElement {
     public static void main(String[] args) {
         int []a={1,3,5,6,23,34,45};
+        int k=2;
+       Arrays.sort(a);
+        System.out.println(a[k-1]);
 
 
     }
